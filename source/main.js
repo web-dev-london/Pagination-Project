a7768255d243e9ce3283aa53324353bcf64fe730
Murod Akhmedov <murod.london@gmail.com>
2024-01-06 20:32:17 +0000

    Changed Color

--- source/main.js
+++ source/main.js
@@ -18,6 +18,43 @@ const pageCount = Math.ceil(
 );
 let currentPage = 1;
 
+/* Defined a function to create a new btn for the page number */
+const appendPageNumber = (index) => {
+  const pageNumber = document.createElement('button');
+  pageNumber.className = 'pagination-number';
+  pageNumber.innerHTML = index;
+  pageNumber.setAttribute('page-index', index);
+  pageNumber.setAttribute('aria-label', 'Page ' + index);
+
+  paginationNumbers.appendChild(pageNumber);
+};
+/* & then added the btns to the paginationNumbers contaier. */
+const getPaginationNumbers = () => {
+  for (let i = 1; i <= pageCount; i++) {
+    appendPageNumber(i);
+  }
+};
+
+/* Set the value of currentPage variable to the pageNum value */
+const setCurrentPage = (pageNum) => {
+  currentPage = pageNum;
+
+  handleActivePageNumber(); // included, when active page number updated & new page will be set.
+  handlePageButtonsStatus(); // included, when active page number updated & new page will be set.
+
+  // Get range of items to be shown
+  const prevRange = (pageNum - 1) * paginationLimit;
+  const currRange = pageNum * paginationLimit;
+
+  // Loop through the list of items to be displayed & hide all the items
+  listItems.forEach((item, index) => {
+    item.classList.add('hidden');
+    if (index >= prevRange && index < currRange) {
+      item.classList.remove('hidden');
+    }
+  });
+};
+
 /* Function disableButton */
 const disableButton = (button) => {
   button.classList.add('disabled');
@@ -60,42 +97,6 @@ const handleActivePageNumber = () => {
     });
 };
 
-/* Defined a function to create a new btn for the page number */
-const appendPageNumber = (index) => {
-  const pageNumber = document.createElement('button');
-  pageNumber.className = 'pagination-number';
-  pageNumber.innerHTML = index;
-  pageNumber.setAttribute('page-index', index);
-  pageNumber.setAttribute('aria-label', 'Page ' + index);
-
-  paginationNumbers.appendChild(pageNumber);
-};
-/* & then added the btns to the paginationNumbers contaier. */
-const getPaginationNumbers = () => {
-  for (let i = 1; i <= pageCount; i++) {
-    appendPageNumber(i);
-  }
-};
-
-/* Set the value of currentPage variable to the pageNum value */
-const setCurrentPage = (pageNum) => {
-  currentPage = pageNum;
-
-  handleActivePageNumber(); // included, when active page number updated & new page will be set.
-  handlePageButtonsStatus(); // included, when active page number updated & new page will be set.
-
-  // Get range of items to be shown
-  const prevRange = (pageNum - 1) * paginationLimit;
-  const currRange = pageNum * paginationLimit;
-
-  // Loop through the list of items to be displayed & hide all the items
-  listItems.forEach((item, index) => {
-    item.classList.add('hidden');
-    if (index >= prevRange && index < currRange) {
-      item.classList.remove('hidden');
-    }
-  });
-};
 /* Called getPaginationNumbers in a window.load() event */
 window.addEventListener('load', () => {
   getPaginationNumbers();
